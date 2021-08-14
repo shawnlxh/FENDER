4865d81f444e8cf506184912f4488e7c58874ae5
shawnlxh <shawnlxh@outlook.com>
2021-08-13 20:25:54 -0500

    Add files via upload

---
+++ Model/utility/load_data.py
@@ -0,0 +1,215 @@
+import collections
+import numpy as np
+import random as rd
+import heapq
+
+class Data(object):
+    def __init__(self, args, path):
+        self.path = path
+        self.args = args
+
+        self.batch_size = args.batch_size
+        if args.model_type == 'fender':
+            train_file = path + '/train.txt'
+        if args.model_type == 'bprmf':
+            train_file = path + '/train_bpr.txt'
+        test_file = path + '/test.txt'
+        frequency_file = path + "/fre_test.txt"
+
+
+        # ----------get number of users and items & then load rating data from train_file & test_file------------.
+        self.n_train, self.n_test = 0, 0
+        self.n_users, self.n_items = 0, 0
+        
+        self.k = 10 #find top 10 frequent items
+
+        self.train_data, self.train_user_dict = self._load_ratings(train_file)
+        self.test_data, self.test_user_dict = self._load_ratings(test_file)
+        # user_item: all item purchased by the user.
+        # user_frequency: top-k frequent items in all purchased items
+        self.user_item, self.user_top_fre = self._load_frequency(frequency_file, self.k)
+        self.exist_users = self.train_user_dict.keys()
+
+        self._statistic_ratings()
+
+        # ----------get number of entities and relations & then load kg data from kg_file ------------.
+    
+
+        # ----------print the basic info about the dataset-------------.
+
+        self._print_data_info()
+
+    # reading train & test interaction data.
+    def _load_ratings(self, file_name):
+        user_dict = dict()
+        inter_mat = list()
+
+        lines = open(file_name, 'r').readlines()
+        for l in lines:
+            tmps = l.strip()
+            inters = [int(i) for i in tmps.split(' ')]
+
+            u_id, pos_ids = inters[0], inters[2:]
+            pos_ids = list(set(pos_ids))
+
+            for i_id in pos_ids:
+                inter_mat.append([u_id, i_id])
+
+            if len(pos_ids) > 0:
+                user_dict[u_id] = pos_ids
+        return np.array(inter_mat), user_dict
+    
+    def _load_frequency(self, file_name, k):
+        f_fre_test = open(file_name,'r')
+        user_item = dict()
+        user_fre = dict()
+        for line in f_fre_test:
+            line = line.split(' ')
+            user = int(line[0])
+            item = int(line[1])
+            fre = float(line[2])
+            if user not in user_item.keys():
+                user_item[user] = [item]
+                user_fre[user] = [fre]
+            else:
+                user_item[user].append(item)
+                user_fre[user].append(fre)
+
+        user_top_fre = dict()
+        for user in user_item.keys():
+            items = user_item[user]
+            fre = user_fre[user]
+            item_fre = dict()
+            for i, item in enumerate(items):
+                item_fre[item] = fre[i]
+
+            top_fre = heapq.nlargest(k, item_fre, key = item_fre.get)
+            user_top_fre[user] = top_fre
+            
+        return user_item, user_top_fre
+
+    def _statistic_ratings(self):
+        self.n_users = max(max(self.train_data[:, 0]), max(self.test_data[:, 0])) + 1
+        self.n_items = max(max(self.train_data[:, 1]), max(self.test_data[:, 1])) + 1
+        self.n_train = len(self.train_data)
+        self.n_test = len(self.test_data)
+
+    # reading train & test interaction data.
+
+    def _print_data_info(self):
+        print('[n_users, n_items]=[%d, %d]' % (self.n_users, self.n_items))
+        print('[n_train, n_test]=[%d, %d]' % (self.n_train, self.n_test))
+        
+        
+
+    def _generate_train_cf_batch(self):
+        if self.batch_size <= self.n_users:
+            users = rd.sample(self.exist_users, self.batch_size)
+        else:
+            users = [rd.choice(self.exist_users) for _ in range(self.batch_size)]
+
+        def sample_pos_items_for_u(u, num):
+            pos_items = self.train_user_dict[u]
+            n_pos_items = len(pos_items)
+            pos_batch = []
+            while True:
+                if len(pos_batch) == num: break
+                pos_id = np.random.randint(low=0, high=n_pos_items, size=1)[0]
+                pos_i_id = pos_items[pos_id]
+
+                if pos_i_id not in pos_batch:
+                    pos_batch.append(pos_i_id)
+            return pos_batch
+
+        def sample_neg_items_for_u(u, num):
+            neg_items = []
+            while True:
+                if len(neg_items) == num: break
+                neg_i_id = np.random.randint(low=0, high=self.n_items,size=1)[0]
+
+                if neg_i_id not in self.train_user_dict[u] and neg_i_id not in neg_items:
+                    neg_items.append(neg_i_id)
+            return neg_items
+
+        pos_items, neg_items = [], []
+        for u in users:
+            pos_items += sample_pos_items_for_u(u, 1)
+            neg_items += sample_neg_items_for_u(u, 1)
+
+        return users, pos_items, neg_items
+
+    def get_sparsity_split(self):
+        try:
+            split_uids, split_state = [], []
+            lines = open(self.path + '/sparsity.split', 'r').readlines()
+
+            for idx, line in enumerate(lines):
+                if idx % 2 == 0:
+                    split_state.append(line.strip())
+                    print(line.strip())
+                else:
+                    split_uids.append([int(uid) for uid in line.strip().split(' ')])
+            print('get sparsity split.')
+
+        except Exception:
+            split_uids, split_state = self.create_sparsity_split()
+            f = open(self.path + '/sparsity.split', 'w')
+            for idx in range(len(split_state)):
+                f.write(split_state[idx] + '\n')
+                f.write(' '.join([str(uid) for uid in split_uids[idx]]) + '\n')
+            print('create sparsity split.')
+
+        return split_uids, split_state
+
+
+
+    def create_sparsity_split(self):
+        all_users_to_test = list(self.test_user_dict.keys())
+        user_n_iid = dict()
+
+        # generate a dictionary to store (key=n_iids, value=a list of uid).
+        for uid in all_users_to_test:
+            train_iids = self.train_user_dict[uid]
+            test_iids = self.test_user_dict[uid]
+
+            n_iids = len(train_iids) + len(test_iids)
+
+            if n_iids not in user_n_iid.keys():
+                user_n_iid[n_iids] = [uid]
+            else:
+                user_n_iid[n_iids].append(uid)
+        split_uids = list()
+
+        # split the whole user set into four subset.
+        temp = []
+        count = 1
+        fold = 4
+        n_count = (self.n_train + self.n_test)
+        n_rates = 0
+
+        split_state = []
+        for idx, n_iids in enumerate(sorted(user_n_iid)):
+            temp += user_n_iid[n_iids]
+            n_rates += n_iids * len(user_n_iid[n_iids])
+            n_count -= n_iids * len(user_n_iid[n_iids])
+
+            if n_rates >= count * 0.25 * (self.n_train + self.n_test):
+                split_uids.append(temp)
+
+                state = '#inter per user<=[%d], #users=[%d], #all rates=[%d]' %(n_iids, len(temp), n_rates)
+                split_state.append(state)
+                print(state)
+
+                temp = []
+                n_rates = 0
+                fold -= 1
+
+            if idx == len(user_n_iid.keys()) - 1 or n_count == 0:
+                split_uids.append(temp)
+
+                state = '#inter per user<=[%d], #users=[%d], #all rates=[%d]' % (n_iids, len(temp), n_rates)
+                split_state.append(state)
+                print(state)
+
+
+        return split_uids, split_state
